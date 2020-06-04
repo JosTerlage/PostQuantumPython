c340be73d92c3c0df030e7300be72ef18e9f20f2
JosTerlage <jos211@live.nl>
2020-06-04 21:20:36 +0200

    AES encryption added

--- clientproxy.py
+++ clientproxy.py
@@ -1,5 +1,11 @@
 import socket
 
+from poc import MiniKyber, Kyber, Nose, Skipper2Negated, Skipper4, BinomialDistribution
+from sage.crypto.mq.rijndael_gf import RijndaelGF
+
+#AES Block size 128, key length 256
+rgf = RijndaelGF(4, 6)
+
 #Init sockets
 clientSideSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 clientSideSocket.bind((socket.gethostname(), 8090))
@@ -7,23 +13,39 @@ clientSideSocket.listen(5)
 clientsocket, address = clientSideSocket.accept()
 print("Connection has been established")
 
-while True:
-    msg = clientsocket.recv(1024)
-    print("Message has been received")
+try:
+    while True:
+        msg = clientsocket.recv(1024)
+        print("Message has been received")
 
-    #send message to serverproxy
-    serverproxySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
-    serverproxySocket.connect(("stempoljos.westeurope.cloudapp.azure.com", 8092))
-    serverproxySocket.send(bytes(msg))
+        #Connect with serverproxy 
+        serverproxySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
+        serverproxySocket.connect(("stempoljos.westeurope.cloudapp.azure.com", 8092))
 
-    #listen for response
-    response = serverproxySocket.recv(1024)
-    clientsocket.send(bytes(response))
+        #Wait for PK from server
+        serverproxyPK = serverproxySocket.recv(1024)
+        serverproxyPK = serverproxyPK.decode("utf-8")
+
+        #Generate AES key
+        aesKey = "000102030405060708090a0b0c0d0e0f1011121314151617"
+
+        #Encrypt AES with Post Quantum PK from server and send to serverproxy
+        encryptedAesKey = Kyber.enc(serverproxyPK, m=aesKey)
+        serverproxySocket.send(bytes(encryptedAesKey))
+
+        #send message to serverproxy with AES
+        aesEncryptedMsg = rgf.encrypt(msg, aesKey)
+        serverproxySocket.send(bytes(aesEncryptedMsg))
+
+        #listen for response and decrypt with AES key
+        response = serverproxySocket.recv(1024)
+        response = rgf.decrypt(response, aesKey)
+        clientsocket.send(bytes(response))
 
 
-    serverproxySocket.close()
-
-clientsocket.close()
-clientSideSocket.close()
+        serverproxySocket.close()
+except socket.timeout:
+    clientsocket.close()
+    clientSideSocket.close()
 
 
--- serverproxy.py
+++ serverproxy.py
@@ -1,5 +1,11 @@
 import socket
 
+from poc import MiniKyber, Kyber, Nose, Skipper2Negated, Skipper4, BinomialDistribution
+from sage.crypto.mq.rijndael_gf import RijndaelGF
+
+rgf = RijndaelGF(4, 6)
+
+
 #Init sockets
 serverproxySocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serverproxySocket.bind((socket.gethostname(), 8092))
@@ -9,16 +15,33 @@ serversideSocket = socket.socket(socket.AF_INET, socket.SOCK_STREAM)
 serversideSocket.connect((socket.gethostname(), 8094))
 
 while True:
+    #Establish connection with clientproxy
     clientproxySocket, address = serverproxySocket.accept()
     print("Tunnel has been established")
+
+    #Generate Post Quantum Keypair
+    serverproxyPublickey, serverproxyPrivatekey = Kyber.key_gen()
+
+    #Send Post Quantum Public key to clientproxy
+    clientproxySocket.send(bytes(serverproxyPublickey, "utf-8"))
+
+    #Wait for Post Quantum encrypted AES key and decrypt
+    aesKey = clientproxySocket.recv(1024)
+    aesKey = Kyber.dec(serverproxyPrivatekey, c=aesKey)
+
+    #Wait for message encrypted with AES and decrypt   
     msg = clientproxySocket.recv(1024)
+    msg = rgf.decrypt(msg, aesKey)
 
     #send message to serverside
     serversideSocket.send(bytes(msg))
 
     #wait for response
     response = serversideSocket.recv(1024)
-    clientproxySocket.send(bytes(response))
+
+    #Encrypt response with AES key and send to clientproxy
+    encryptedresponse = rgf.encrypt(response, aesKey)
+    clientproxySocket.send(bytes(encryptedresponse))
     clientproxySocket.close()
 
     
